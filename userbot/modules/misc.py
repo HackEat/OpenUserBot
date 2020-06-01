01fa609b230798054863feb23e2fa8996bafd9ef
HackEat <60584815+HackEat@users.noreply.github.com>
2020-06-02 02:40:46 +0530

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -149,31 +149,7 @@ async def raw(event):
             reply_to=reply_to_id,
             caption="`Here's the decoded message data !!`")
 
-        
-QUOTES = [
-    "Using Witchery to kang this sticker...",
-    "Plagiarising hehe...",
-    "Inviting this sticker over to my pack...",
-    "Kanging this sticker...",
-    "Hey that's a nice sticker!\nMind if I kang?!..",
-    "1,2,3 Boom. Your Sticker Has Betrayed You.",
-    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
-    "Roses are red violets are blue, kanging this sticker so my pacc looks cool",
-    "Taking This Sticker Under My Remand...",
-    "That's A Lit Sticker Which You Don't Deserve,So NVM I'll Take It... ",
-]
 
-@register(outgoing=True, pattern="^.quote$")
-async def quote(msgs):
-    """ For .quote command, check if the bot is running.  """
-    await msgs.edit(f"{random.choice(QUOTES)}")
-                     
-
-CMD_HELP.update({
-    "quotes":
-    ".quote\
-\nUsage: Type `.quote` to receive a random quote."
-})
         
 CMD_HELP.update({
     'random':
